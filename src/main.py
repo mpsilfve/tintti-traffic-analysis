d8e5e824399e7f0ef35f657c0699e13739723a88
Miikka Silfverberg <msilfver@mail.ubc.ca>
2024-11-19 13:04:29 +0200

    Plot graph of captured traffic per time.

--- src/main.py
+++ src/main.py
@@ -4,9 +4,16 @@ import click
 
 @click.command()
 @click.option("--input-file", required=True, help="Captured traffic file")
-def main(input_file):
+@click.option("--traffic-graph-output-file", required=True, help="Store the generated traffic/time graph into this file.")
+def main(input_file,
+         traffic_graph_output_file):
     print(f"Analyzing file {input_file}")
     capture = pyshark.FileCapture(input_file)
-
+    print("Capture info:")
+    print(capture)
+    
+    # Generate graph of traffic as a function of time
+    traffic_analysis.generate_traffic_graph(capture, traffic_graph_output_file)
+    
 if __name__=="__main__":
     main()
