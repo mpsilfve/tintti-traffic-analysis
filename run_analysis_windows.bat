246a021ae6f9a4f866036616a437b664df6556fd
Miikka Silfverberg <msilfver@mail.ubc.ca>
2024-11-21 14:43:12 +0200

    Added plotting of data per second graph in addition to packets per second graph.

--- run_analysis_windows.bat
+++ run_analysis_windows.bat
@@ -6,5 +6,6 @@ if not exist "generated" mkdir generated
 REM Run the Python script
 python src\main.py --input-file data\liikenne.pcapng ^
                    --traffic-packets-main-statistics generated\packets_statistics.txt ^
-                   --traffic-graph-output-file generated\traffic_graph.png ^
+                   --packet-graph-output-file generated/packet_graph.png ^
+                   --data-graph-output-file generated/data_graph.png ^
                    --traffic-packets-histogram generated\traffic_packets_histogram.png
